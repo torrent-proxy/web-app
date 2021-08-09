d5f92d57304adb4ad309e0c55da1062aabc96844
kicumkicum <kicumkicum@gmail.com>
2021-08-09 18:15:29 +0300

    Create pages and pseudo-router temporary

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -1,23 +1,46 @@
+// @ts-nocheck
 import * as React from "react";
-import DownloadButton from "../download-button/download-button.component";
-import Header from "../header/header.component";
-import SearchBox from "../search-box/search-box.component";
-import SearchResults from "../search-results/search-results.component";
+import SearchResultsPage from "../pages/search-results/search-results.component";
+import SearchPage from "../pages/search/search.component";
 
-const App: React.FunctionComponent = () => {
-  const onDownloadButtonClick = () => {};
-  const onDownloadButtonHover = () => {};
+// TODO: Use react-router instead of this
+const PseudoRouter = (props) => {
+  const {children, childPosition} = props;
+  const child = React.Children.toArray(children)[childPosition];
 
   return (
-    <React.Fragment>
-      <Header />
-      <SearchBox />
-      <SearchResults />
-      <DownloadButton
-        onDownloadButtonClick={onDownloadButtonClick}
-        onDownloadButtonHover={onDownloadButtonHover}
-      />
-    </React.Fragment>
+    <div>
+      {child}
+    </div>
+  );
+};
+
+const Route = (props) => {
+  const {children} = props;
+
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
+
+const App: React.FunctionComponent = () => {
+  return (
+    <PseudoRouter
+      childPosition={0}
+    >
+      <Route
+        src={"/results/:id"}
+      >
+        <SearchResultsPage />
+      </Route>
+      <Route
+        src={"/search"}
+      >
+        <SearchPage />
+      </Route>
+    </PseudoRouter>
   );
 };
 
---
+++ src/components/pages/search/search.component.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import SearchBox from "../../search-box/search-box.component";
+
+const SearchPage: React.FunctionComponent = () => {
+  debugger
+  return (
+    <React.Fragment>
+      <SearchBox />
+    </React.Fragment>
+  );
+};
+
+export default SearchPage;
